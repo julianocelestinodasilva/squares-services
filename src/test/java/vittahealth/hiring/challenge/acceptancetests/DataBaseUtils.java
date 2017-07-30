7c06a9c96a8595cddce1d0c982386202febc4e61
Juliano Celestino <julianocelestinodasilva@gmail.com>
2017-07-30 19:14:54 -0300

    list_territories_ordered_by_most_proportional_painted_area  -  DOING

--- src/test/java/vittahealth/hiring/challenge/acceptancetests/DataBaseUtils.java
+++ src/test/java/vittahealth/hiring/challenge/acceptancetests/DataBaseUtils.java
@@ -12,14 +12,28 @@ import java.util.List;
 
 public class DataBaseUtils {
 
+    public static List<Territory> persistTerritories(Territory territoryA, Territory territoryB) throws IOException {
+        List<Territory> territories = new ArrayList<Territory>();
+        territoryA.setPaintedArea(0L);
+        territories.add(territoryA);
+        territories.add(territoryB);
+        final Session session = JPAUtil.session();
+        session.getTransaction().begin();
+        session.createNativeQuery("DELETE FROM Territory").executeUpdate();
+        for (Territory territory : territories) {
+            session.persist(territory);
+            session.flush();
+        }
+        session.getTransaction().commit();
+        return territories;
+    }
+
     public static List<Territory> persistTerritories(Long paintedAreaTerritory1) throws IOException {
         List<Territory> territories = new ArrayList<Territory>();
         Territory territoryA = new Territory("A",new Node(0,0),new Node(50,50));
-        // territoryA.setArea(2500L);
         territoryA.setPaintedArea(0L);
         territories.add(territoryA);
         Territory territoryB = new Territory("B",new Node(51,51),new Node(101,101));
-        // territoryB.setArea(2500L);
         territoryB.setPaintedArea(paintedAreaTerritory1);
         territories.add(territoryB);
         final Session session = JPAUtil.session();
@@ -36,11 +50,9 @@ public class DataBaseUtils {
     public static List<Territory> persistTerritories() throws IOException {
         List<Territory> territories = new ArrayList<Territory>();
         Territory territoryA = new Territory("A",new Node(0,0),new Node(50,50));
-        // territoryA.setArea(2500L);
         territoryA.setPaintedArea(0L);
         territories.add(territoryA);
         Territory territoryB = new Territory("B",new Node(51,51),new Node(101,101));
-        // territoryB.setArea(2500L);
         territoryB.setPaintedArea(0L);
         territories.add(territoryB);
         final Session session = JPAUtil.session();
--- src/test/java/vittahealth/hiring/challenge/acceptancetests/TerritoryEndPointTest.java
+++ src/test/java/vittahealth/hiring/challenge/acceptancetests/TerritoryEndPointTest.java
@@ -3,6 +3,7 @@ package vittahealth.hiring.challenge.acceptancetests;
 
 import org.junit.Before;
 import org.junit.Test;
+import vittahealth.hiring.challenge.domain.Node;
 import vittahealth.hiring.challenge.domain.Territory;
 
 import java.util.List;
@@ -29,7 +30,12 @@ public class TerritoryEndPointTest {
 
     @Test
     public void list_territories_ordered_by_most_proportional_painted_area() throws Exception {
-        throw new RuntimeException("Implementar");
+        territory0 = new Territory("territory0",new Node(0,0),new Node(10,10));
+        territory0.setPaintedArea(95L);
+        territory1 = new Territory("territory0",new Node(0,0),new Node(50,50));
+        territory1.setPaintedArea(2500L);
+        territories = DataBaseUtils.persistTerritories(territory1,territory0);
+        expectTerritoryZeroFirst();
     }
 
     @Test
@@ -41,7 +47,7 @@ public class TerritoryEndPointTest {
         logger.log(Level.INFO, url);
         expect().statusCode(200).
                 body("size()", is(territories.size())).
-                body("get(0).id", equalTo(2)).
+                body("get(0).id", equalTo(territory1.getId().intValue())).
                 body("get(0).name", equalTo(territory1.getName())).
                 body("get(0).startArea.x", equalTo(territory1.getStartArea().getX())).
                 body("get(0).startArea.y", equalTo(territory1.getStartArea().getY())).
@@ -49,7 +55,7 @@ public class TerritoryEndPointTest {
                 body("get(0).endArea.y", equalTo(territory1.getEndArea().getY())).
                 body("get(0).area", equalTo(territory1.getArea().intValue())).
                 body("get(0).paintedArea", equalTo(territory1.getPaintedArea().intValue())).
-                body("get(1).id", equalTo(1)).
+                body("get(1).id", equalTo(territory0.getId().intValue())).
                 body("get(1).name", equalTo(territory0.getName())).
                 body("get(1).startArea.x", equalTo(territory0.getStartArea().getX())).
                 body("get(1).startArea.y", equalTo(territory0.getStartArea().getY())).
@@ -65,10 +71,14 @@ public class TerritoryEndPointTest {
         territories = DataBaseUtils.persistTerritories();
         territory0 = territories.get(0);
         territory1 = territories.get(1);
+        expectTerritoryZeroFirst();
+    }
+
+    private void expectTerritoryZeroFirst() {
         logger.log(Level.INFO, url);
         expect().statusCode(200).
                 body("size()", is(territories.size())).
-                body("get(0).id", equalTo(1)).
+                body("get(0).id", equalTo(territory0.getId().intValue())).
                 body("get(0).name", equalTo(territory0.getName())).
                 body("get(0).startArea.x", equalTo(territory0.getStartArea().getX())).
                 body("get(0).startArea.y", equalTo(territory0.getStartArea().getY())).
@@ -76,7 +86,7 @@ public class TerritoryEndPointTest {
                 body("get(0).endArea.y", equalTo(territory0.getEndArea().getY())).
                 body("get(0).area", equalTo(territory0.getArea().intValue())).
                 body("get(0).paintedArea", equalTo(territory0.getPaintedArea().intValue())).
-                body("get(1).id", equalTo(2)).
+                body("get(1).id", equalTo(territory1.getId().intValue())).
                 body("get(1).name", equalTo(territory1.getName())).
                 body("get(1).startArea.x", equalTo(territory1.getStartArea().getX())).
                 body("get(1).startArea.y", equalTo(territory1.getStartArea().getY())).
